70ca7e049ed95812020b8626b4b536fd6bbadb5f
Chandhana <88320292+ChandhanaReddy@users.noreply.github.com>
2022-03-01 16:07:11 +0530

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,49 @@
+import Image from "next/image";
+import styles from "../styles/Navabar.module.css";
+
+const items = [
+  "Home",
+  "Products",
+  "Menu",
+  "Events",
+  "Blog",
+  "Contact",
+  "Chinku's \nPizza World",
+];
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callBtn}>
+          <Image src={"/assets/telephone.png"} width={"30px"} height={"30px"} />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>+91 999-999-9999</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          {items.map((item, index) => (
+            <>
+              {index === 6 ? (
+                <li className={styles.logoTxt}>{item}</li>
+              ) : (
+                <li key={index} className={styles.listItem}>
+                  {item}
+                </li>
+              )}
+            </>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/assets/cart.png" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Navbar;
